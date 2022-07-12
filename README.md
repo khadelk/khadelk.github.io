3f273f1e72b4051fed0ec91aa04e871aa4d16983
kelkeurti <kelkeurti@uiowa.edu>
2022-07-12 07:33:03 -0500

    Sveltekit site

--- README.md
+++
@@ -1,1 +0,0 @@
-# khadelk.github.io
--- sveltekit/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/fallbacks/error.svelte.js';
-
-export { module };
-export const index = 1;
-export const entry = 'error.svelte-7f6753eb.js';
-export const js = ["error.svelte-7f6753eb.js","chunks/index-b5b9e693.js"];
-export const css = [];
